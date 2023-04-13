f50fd51bf05298900ae3badc4fe8d1fb46368917
Isham_Cespedes <ishamscespedes@gmail.com>
2023-04-13 09:34:29 -0500

    first commit

---
+++ src/routes/auth/login.js
@@ -0,0 +1,41 @@
+const axios = require( 'axios' );
+
+module.exports = async (req, res) => {
+
+	console.log(req.body);
+	res.send("Login");
+    
+    try {
+        const mutation = `
+            mutation ($email: String!, $email: String!, $password: String!){
+                login(
+                    email: $email
+                    password: $password
+                )
+            }
+        `
+
+        const { data } = await axios.post(process.env.GRAPHQL_ENDPOINT,
+                {
+                query: mutation,
+                variables: {
+                    email: req.body.email,
+                    password: req.body.password
+                }
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+
+            const jwtoken = data.data.register;
+            res.cookie('jwtoken', jwtoken, { httpOnly: true })
+            res.redirect('/');
+
+        } catch(err){
+            console.log(err)
+            res.redirect('/auth/register')
+        }
+    }
